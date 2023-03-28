2d40c085aeca54cd6859cbb2cf7f834156fc92df
kerenmevorach <mevok873@newschool.edu>
2023-03-28 16:38:00 -0400

    structured website. added copy to word of the day and glossary.

---
+++ assets/script.js
@@ -0,0 +1,21 @@
+//make a j.son file
+
+// Word of the day: 
+//each day at 12:00AM, pick a random number
+//random number * length of json file list
+//whatever that number is - display that number term, definition, and example on the page. 
+//eg. random number is 9, so display the 9th term and its info
+
+// Translator:
+//create text user input
+// Loooooooong conditional with all the terms and their respective info
+// If user input is ___, the definition is ___ and example is ____. 
+// Is there any way to do this with the j.son file? 
+//Look up the word in the json file, find the corresponding definition and example. 
+
+// Glossary: 
+//loop through each set of header, definition and example.
+//term[0]
+//definition[0]
+//example[0]
+//etc.
