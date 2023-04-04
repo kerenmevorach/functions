ee2181e577ae321a19073d4439f0bdbd3209bb52
kerenmevorach <mevok873@newschool.edu>
2023-04-03 23:50:46 -0400

    styling of translator page

--- assets/script.js
+++ assets/script.js
@@ -10,6 +10,7 @@
 //create text user input
 // Loooooooong conditional with all the terms and their respective info
 // If user input is ___, the definition is ___ and example is ____. 
+// If user input = term[0], the definition is definition[0] and example is example[0]. 
 // Is there any way to do this with the j.son file? 
 //Look up the word in the json file, find the corresponding definition and example. 
 
@@ -19,3 +20,13 @@
 //definition[0]
 //example[0]
 //etc.
+
+//jk nvm
+//loop through each set of terms, display that on the page.
+//if the user clicks on a specific term, term[i], then add definition[i], and exmaple etc. 
+//basically have all the terms - and if that term is opened, display corresponding info 
+//maybe make a template in html, and have it be visible vs not 
+//create list
+//add list items - title, def, ex
+//when list title is clicked, add 2 more list items 
+//is there a pop up with the definition?
