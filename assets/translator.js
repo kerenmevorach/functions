7d9f85018e9d2209ec01ae6a5d710294d9e5a305
kerenmevorach <mevok873@newschool.edu>
2023-04-11 14:21:25 -0400

    got input text to appear on page (not from json)

--- assets/translator.js
+++ assets/translator.js
@@ -1,29 +1,30 @@
 // Function to render your items
 const renderItems = (glossary) => {
 	// The `ul` where the items will be inserted
-    const translatedTermList = document.querySelector('#translated-term-container');
+
     const translateButton = document.querySelector('.translate-button');
-    const termToTranslate = document.querySelector('#term-to-translate').value;
+    const translatedTermList = document.querySelector('#translated-term-container');
+    //for some reason this needs to be repeated here and within the function for the list item to appear in the dom
+    let termToTranslate = document.querySelector('#term-to-translate').value;
 
     
-    // on click, run the translate function
-    translateButton.onclick = () =>{
-        translateTerm();
-        console.log('translate');
-    }
-
     function translateTerm() {
-        console.log('translate');
 
-        //clear what's already in the innterHTML
-        // translatedTermList.innerHTML = ''
+        //clear any previous searches
+        translatedTermList.innerHTML = ''
 
+        var termToTranslate = document.querySelector('#term-to-translate').value
+        console.log(termToTranslate)
+        const listItem = document.createElement('li') 
+        listItem.innerHTML = termToTranslate
+        translatedTermList.appendChild(listItem)
+    
         // Loop through each item in the collection array
 	    glossary.forEach(item => {
 
-            myArray = JSON.parse(glossary);
-            myObj.title[0];
-            console.log(myObj.title[0])
+            // myArray = JSON.parse(glossary);
+            // myObj.title[0];
+            // console.log(myObj.title[0])
 
             for (let i in item.tags) {
                 //this for loop converts the input text, finds the corresponding image, and displays it on the page
@@ -48,64 +49,17 @@ const renderItems = (glossary) => {
             
                     //Add the list item to the ul or translated terms list
                     translatedTermList.appendChild(listItem) // Then add the whole `li` into the `ul`
-                }
-              }
-	})
+                    }
+                 }
+	            })
+
+            }
 
         // on click, run the translate function
-        //translate function:
-        //for each term, loop through the glossary json, or loop through the tags
-        //if the searched term matches a tag,
-        //display the name of the term (list item)
-        //display the definition (list item)
-        //display the americanized version
-
-
-
-
-        for (var i of termToTranslate.toLowerCase()) {
-
-
-            myArray = JSON.parse(glossary);
-            item.title[0];
-            console.log(item.title[0])
-            
-
-
-            //this for loop converts the input text, finds the corresponding image, and displays it on the page
-            // if (termToTranslate.includes(i)) {
-            //   //make a list item
-            // //   glossary.forEach(item => {
-
-         
-
-            //   const listItem = document.createElement('li')
-
-            //   const itemDetails =
-            //       `
-            //           <h2 class="term">${item.title}</h2>
-            //           <h3>Definition:</h3>
-            //           <p class="definition">${item.definition}</p>
-            //           <h3>Americanized:</h3>
-            //           <p>${item.americanized}</p>
-            //           <div class="line"></div>
-            //       `
-      
-            //   listItem.insertAdjacentHTML('beforeend', itemDetails) // Which can we then insert
-            //   listItem.classList.add('word-set')
-      
-            //   translatedTermList.appendChild(listItem) // Then add the whole `li` into the `ul`
-
-
-            //   document.getElementById(
-            //     'inputContainer',
-            //   ).innerHTML += `<img src="images/${i}.png"  class=letterimages>`
-            // //   })
-            // }
-          }
-
-    }
-
+        translateButton.onclick = () =>{
+            translateTerm();
+            console.log('translate');
+        }
 }
 
 // Fetch gets your JSON file…
@@ -116,3 +70,11 @@ fetch('assets/glossary.json')
 		renderItems(glossary) // In order
 	})
 
+
+        // on click, run the translate function
+        //translate function:
+        //for each term, loop through the glossary json, or loop through the tags
+        //if the searched term matches a tag,
+        //display the name of the term (list item)
+        //display the definition (list item)
+        //display the americanized version
