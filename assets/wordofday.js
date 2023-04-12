ac82eda039e228b42edb0cd9ab0c99e04e2b0991
kerenmevorach <mevok873@newschool.edu>
2023-04-11 23:00:32 -0400

    term resets at midnight

--- assets/wordofday.js
+++ assets/wordofday.js
@@ -1,71 +1,37 @@
 let datafile = "assets/glossary.json";
 
 const wordContainer = document.querySelector('#wordofdaycontainer');
+const term = document.querySelector('.term');
+const americanVersion = document.querySelector('.american-version');
+const termDefinition = document.querySelector('.term-definition');
 let itemDetails;
+
 
 $(function() { 
 
     //get file  
     $.getJSON(datafile, function(data) {
-
     
-            const listItem = document.createElement('li') 
+        term.innerHTML = data[0].title
+        americanVersion.innerHTML = data[0].americanized
+        termDefinition.innerHTML = data[0].definition
 
-            //Create the item details
-            itemDetails =
-                `
-                    <h1 class = "current">${data[0].title}</h1>
-                    <h2>Americanized:</h2>
-                    <p>${data[0].americanized}</p>
-                    <h2>Definition:</h2>
-                    <p>${data[0].definition}</p>
-                `;
-            
-            //add the item details to the list item
-            listItem.innerHTML = itemDetails
-            
-            //Add the list item to the ul / translated terms list
-            wordContainer.appendChild(listItem) // Then add the whole `li` into the `ul`
-
-        
         function getTime() {
             let h = new Date().getHours()
             let m = new Date().getMinutes()
             let s = new Date().getSeconds()
-
-            // let h = 22;
-            // let m = 32;
-            // let s = ;
             
             let time = h + ":" + m;
             console.log(time)
 
-        if (h == 22 && m == 50 && s == 0) {
-            console.log('hi')
+        if (h == 0 && m == 0 && s == 0) {
+            console.log('its time!')
 
             let randomNumber = Math.floor(Math.random() * 21)
 
-            // wordContainer.removeChild(listItem)
-            // block.classList.remove('current')
-
-            const listItem = document.createElement('li') 
-            
-            //Create the item details
-            // wordContainer.removeChild(listItem)
-            let itemDetails =
-                `
-                    <h1>${data[randomNumber].title}</h1>
-                    <h2>Americanized:</h2>
-                    <p>${data[randomNumber].americanized}</p>
-                    <h2>Definition:</h2>
-                    <p>${data[randomNumber].definition}</p>
-                `;
-            
-            //add the item details to the list item
-            listItem.innerHTML = itemDetails
-            
-            //Add the list item to the ul / translated terms list
-            wordContainer.appendChild(listItem) // Then add the whole `li` into the `ul`
+            term.innerHTML = data[randomNumber].title
+            americanVersion.innerHTML = data[randomNumber].americanized
+            termDefinition.innerHTML = data[randomNumber].definition
         }
         
     };
@@ -73,33 +39,5 @@ $(function() {
     setInterval(getTime, 1000)
                 
     });
+    
 });
-
-
-
-
-
-    // if (h >= 0 && m >= 0 && s >= 0) {
-        //     console.log('hi')
-        // }
-            
-
-        // console.log(data[0].title)
-        // console.log(data.title)
-
-        
-
-        // console.log(randomNumber)
-
-
-        // for (let i = 0; i < data.length; i++ ){
-        //     let terms = [data[i].title]
-        //     // console.log(terms)
-        //     console.log(terms.length);
-        //     console.log(data[randomNumber].title)
-        // }
-
-
-        // Make the `li`
-        
-        // }
