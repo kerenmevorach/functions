3c510729380aad74a9ffbc07d03e9e72d8c934a6
kerenmevorach <mevok873@newschool.edu>
2023-04-25 00:49:37 -0400

    deleted old WOD js

--- assets/wordofday.js
+++ assets/wordofday.js
@@ -1,45 +1,48 @@
+//source: https://www.banjocode.com/post/javascript/iterate-array-with-modulo
+//source: https://www.youtube.com/watch?v=r5Iy3v1co0A
+//source: https://www.kirupa.com/javascript/infinitely_cycle_through_array.htm
+
 let datafile = "assets/glossary.json";
 
 const wordContainer = document.querySelector('#wordofdaycontainer');
 const term = document.querySelector('.term');
 const americanVersion = document.querySelector('.american-version');
 const termDefinition = document.querySelector('.term-definition');
-let itemDetails;
-
 
 $(function() { 
 
     $.getJSON(datafile, function(data) {
-    
-        setWordofDay(data);
 
-        function getTime() {
-            let now = new Date();
-            let h = new Date().getHours()
-            let m = new Date().getMinutes()
-            let s = new Date().getSeconds()
+        function wordOfTheDay() {
+
+            //example from source
+            // let arrayLength = data.length;
+            // let currentIndex = 24;
+            // let nextIndex = (currentIndex + 1) % arrayLength;
+
+            // console.log(today)
+            // console.log(data.length);
+            // console.log(nextIndex);
+
+            // let minute = new Date().getSeconds() - 1;
+            // let wordIndex = Math.abs(minute % data.length);
+     
             
-            let time = h + ":" + m;
-            console.log(time)
+            //getting the day and subtracting one, since the array starts from 0
+            let today = new Date().getDate() - 1;
+            //using modulo operator - ensures we are staying within the array
+            //once the wordIndex is larger than the length of the data, the wordIndex will go back to 0
+            let wordIndex = today % data.length;
+            console.log(wordIndex)
 
-            if (h === 0 && m === 0 && s === 0 ){
-                console.log('it is time!')
-                setWordofDay(data);
-            }
 
-        };
+            term.innerHTML = data[wordIndex].title;
+            termDefinition.innerHTML = data[wordIndex].definition;
+            americanVersion.innerHTML = data[wordIndex].americanized;
+        }
 
-        setInterval(getTime, 60000)
-                
-    });
+        setInterval(wordOfTheDay, 1000);
     
+    });
+
 });
-
-function setWordofDay(data) {
-
-    let randomNumber = Math.floor(Math.random() * data.length);
-
-    term.innerHTML = data[randomNumber].title;
-    termDefinition.innerHTML = data[randomNumber].definition;
-    americanVersion.innerHTML = data[randomNumber].americanized;
-}
